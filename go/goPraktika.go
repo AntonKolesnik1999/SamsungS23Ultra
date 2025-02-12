e9a1e26bdf04503d958bd9439f467188a5f19703
Anton Kolesnik <3020101999@mail.ru>
2025-02-12 13:36:58 +0300

    )

--- go/goPraktika.go
+++ go/goPraktika.go
@@ -2,21 +2,17 @@ package main
 
 import "fmt"
 
-func main() {
-	var a, b, c, d, e, f, g int
-	fmt.Scan(&a)
-
-	b = a / 100000
-	c = (a / 10000) % 10
-	d = (a / 1000) % 10
-	e = (a / 100) % 10
-	f = (a / 10) % 10
-	g = a % 10
-	a = b + c + d
-	b = e + f + g
-	if a == b {
-		fmt.Println("YES")
-	} else {
-		fmt.Println("NO")
-	}
-}
+func main(){
+  
+   var a int
+fmt.Scan (&a)
+   if  a<=0 {
+      fmt.Println ("число R не подходит")
+   } else if a>10000 {
+      fmt.Printf ("%e", a)
+   } else {
+      a=a*a
+      fmt.Println (a)
+   }
+   
+}	
